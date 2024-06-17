a616c1c9128d968b040243082bec171cfc1839fa
Elisee Augustin <eliseeaugustin@gmail.com>
2024-06-16 22:58:18 -0400

    Update api.storage.$id.tsx

--- app/routes/api.storage.$id.tsx
+++ app/routes/api.storage.$id.tsx
@@ -1,6 +1,7 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 
 // Loader Function is used when this route is accessed via a GET method
+//Route for this API: http://localhost:8002/api/storage/{id}
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   //TODO: should do a check/validation for params
 
